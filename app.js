41798f6a45cb376f3b8362cbc3db7f26a6d6a27b
Piotr Szczesniak <piotr.szczesniak@bold.dev>
2021-10-18 20:18:45 +0200

    Korepetycje 18.10.2021

--- app.js
+++ app.js
@@ -1,38 +1,70 @@
 let toDoList = []
+let searchPhrase = ''
+let ascendingOrder = true
+
 const input = document.querySelector('#input')
 const toDoListElement = document.querySelector('.to-do-list')
 const toDosDone = document.querySelector('.to-dos-done')
 const filter = document.querySelector('#filter')
+const sortBtnElement = document.querySelector('#sort')
 
 window.onload = function () {
     render()
 
     input.addEventListener('change', function (e) {
         addItem(e.target.value)
-        const filteredItems = search(filter.value)
 
-        render(filteredItems)
+        render()
     })
 
     filter.addEventListener('keyup', function (e) {
-        const filteredItems = search(e.target.value)
+        searchPhrase = e.target.value
 
-        render(filteredItems)
+        render()
     })
 
     filter.addEventListener('search', function (e) {
         if (!e.target.value) {
+            searchPhrase = ''
+
             render()
         }
+    })
+
+    sortBtnElement.addEventListener('click', function () {
+        sort()
+        ascendingOrder = !ascendingOrder
+        render()
     })
 }
 
 // .sort() a-z
 
+// function(a, b) {
+// if (a.todo > b.todo) { return -1 }
+// }
+
+function sort() {
+    toDoList.sort(function (itemA, itemB) {
+        if (itemA.todo < itemB.todo) {
+            return ascendingOrder ? -1 : 1
+        } else if (itemA.todo > itemB.todo) {
+            return ascendingOrder ? 1 : -1
+        }
+
+        return 0
+    })
+
+    // a < b // return -1 -> [a, b]
+    // a > b // return 1 -> [b, a]
+    // a === b // return 0 -> bez zmian
+}
+
 function search(value) {
     const inputValue = value.toLowerCase()
-    const filteredItems = toDoList.filter((item) =>
-        item.todo.toLowerCase().includes(inputValue)
+    const filteredItems = toDoList.filter(
+        (item) => item.todo.toLowerCase().includes(inputValue)
+        //.match(Regex)
     )
 
     return filteredItems
@@ -66,11 +98,12 @@ function remove(item) {
     render()
 }
 
-function render(list = toDoList) {
+function render() {
+    const filteredItems = search(searchPhrase)
     input.value = ''
     toDoListElement.innerHTML = ''
 
-    list.forEach((item) => {
+    filteredItems.forEach((item) => {
         const itemElement = `
           <li class='to-do-item'>
             ${
@@ -86,6 +119,10 @@ function render(list = toDoList) {
         const liElement = allItemElements[allItemElements.length - 1]
         const removeElement = liElement.querySelector('.remove')
 
+        if (item.done) {
+            liElement.classList.add('to-do-item--done')
+        }
+
         //removing item
         if (removeElement) {
             removeElement.addEventListener('click', function () {
@@ -97,10 +134,6 @@ function render(list = toDoList) {
         liElement.addEventListener('click', function () {
             markAsDone(item)
         })
-
-        if (item.done) {
-            liElement.classList.add('to-do-item--done')
-        }
     })
 
     const done = toDoList.filter(function (item) {
