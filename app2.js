18da6ed959611de7bebdc19d2e34ed5a6c0e4f25
Piotr Szczesniak <piotr.szczesniak@bold.dev>
2021-10-29 21:57:24 +0200

    remake: add sort / add mark done / add mark removed

--- app2.js
+++ app2.js
@@ -1,46 +1,93 @@
+// states
 let toDoList = []
+let sorted = false
 
+// selectors
 const inputElement = document.querySelector('#input')
 const todoListElement = document.querySelector('.to-do-list')
-const todoListItemElement = document.querySelector('to-do-item')
+const sortBtn = document.querySelector('#sort')
 
+// execute
 window.onload = function () {
-    inputElement.addEventListener('change', (e) => {
-        addNewTask(e.target.value)
-    })
+    // add new task
+    inputElement.addEventListener('change', (e) => addNewTask(e.target.value))
+
+    // sort tasks
+    sortBtn.addEventListener('click', () => sortTasks())
 }
 
-function addNewTask(inputValue) {
-    todoListElement.innerHTML = ''
-    let toDo = {
-        todo: inputValue,
-        id: Math.floor(Math.random() * 10000),
-        done: false,
-    }
-    toDoList.push(toDo)
+// declarations
+
+function sortTasks() {
+    sortBtn.innerHTML = `${sorted ? 'Sort A to Z' : 'Sort Z to A'}`
+    toDoList.sort(function (a, b) {
+        if (a.todo > b.todo) {
+            return `${!sorted ? 1 : -1}`
+        }
+        if (a.todo < b.todo) {
+            return `${!sorted ? -1 : 1}`
+        }
+        return 0
+    })
+    sorted = !sorted
     render()
 }
 
-// function markDone() {
+// add new tast
+function addNewTask(inputValue) {
+    todoListElement.innerHTML = ''
+    let item = {
+        todo: inputValue,
+        id: Math.floor(Math.random() * 10000),
+        done: false,
+        removed: false,
+        markDone: function () {
+            this.done = !this.done
+            render()
+        },
+        markRemoved: function () {
+            this.removed = true
+            render()
+        },
+    }
+    toDoList.push(item)
+    render()
+}
 
-// }
-
+// render
 function render() {
+    // clear input and to do list
     inputElement.value = ''
+    todoListElement.innerHTML = ''
+
+    toDoList = toDoList.filter((item) => item.removed === false)
+
     toDoList.forEach((item) => {
         todoListElement.insertAdjacentHTML(
             'beforeend',
-            `<li class="${item.done ? 'to-do-item--done' : 'to-do-item'}">
-                <i class="bi ${
-                    item.done ? 'bi-check2-circle' : 'bi-circle'
-                }"></i>
-                <span class="to-do-item__text">${item.todo}</span>
-                <i class="bi bi-trash remove"></i>
+            `<li class="${
+                item.done ? 'to-do-item--done to-do-item' : 'to-do-item'
+            }">
+                    <i class="bi ${
+                        item.done ? 'bi-check2-circle' : 'bi-circle'
+                    }"></i>
+                    <span class="to-do-item__text">${item.todo}</span>
+                    <i class="bi bi-trash remove"></i>
             </li>`
         )
-        const allItems = document.querySelectorAll('to-do-item')
-        allItems[toDoList.length - 1].addEventListener('click', function () {
-            toDoList.length - 1
+
+        // add click listeners to last li element
+        let allLiElements = todoListElement.querySelectorAll('.to-do-item')
+        let liElement = allLiElements[allLiElements.length - 1]
+
+        // mark done
+        liElement.addEventListener('click', () => item.markDone())
+
+        // mark removed
+        let removeElement = liElement.querySelector('.remove')
+        removeElement.addEventListener('click', (e) => {
+            e.stopPropagation()
+            item.markRemoved()
         })
     })
 }
